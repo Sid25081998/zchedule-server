7b64374364ee1d3e0bab35c3953a572c062ce79b
Sid25081998 <sridharswain25@hotmail.com>
2017-06-11 23:34:43 +0530

    Port changed to Environment port

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
 module.exports= {
-  port: 3000,
+  port: process.env.PORT || 3000,
   captchaUri : "http://academicscc.vit.ac.in/student/captcha.asp",
   loginSubmitAction : 'https://academicscc.vit.ac.in/student/stud_login_submit.asp',
   homeHref : "https://academicscc.vit.ac.in/student/home.asp",
   TimeTableHref: 'https://academicscc.vit.ac.in/student/course_regular.asp?sem=WS'
-  
+
 }
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const config= require('./config');
 
 const app=express();
 const route = require("./routes")(app);
-const port= 3000;//config.port;
+const port= config.port;
 app.listen(port);
 
 console.log("Server has started at "+port);
