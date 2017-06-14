fc0671bf892c6258f01b2795b6f8fe12f16df35e
Sid25081998 <sridharswain25@hotmail.com>
2017-06-14 09:42:47 +0530

    Port changed to 8080

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports= {
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 8080,
   captchaUri : "http://academicscc.vit.ac.in/student/captcha.asp",
   loginSubmitAction : 'https://academicscc.vit.ac.in/student/stud_login_submit.asp',
   homeHref : "https://academicscc.vit.ac.in/student/home.asp",
