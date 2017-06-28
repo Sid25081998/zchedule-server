cea3e2732d716536500781a22b5f97c7dd09c057
Sid25081998 <sridharswain25@hotmail.com>
2017-06-28 10:55:19 +0530

    Cache Test 2

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports= {
   regTextName : 'regno',
   pwdTextName : 'passwd',
   captchaTextName : 'vrfcd',
-  validity : 12*60*60,
+  validity : 12*60,
   captchaUri : "http://academicscc.vit.ac.in/student/captcha.asp",
   loginSubmitAction : 'https://academicscc.vit.ac.in/student/stud_login_submit.asp',
   homeHref : "https://academicscc.vit.ac.in/student/home.asp",
