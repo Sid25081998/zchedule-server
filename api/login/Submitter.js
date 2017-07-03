c4635683e1700273301df462d7d7550aaef3a14b
Sid25081998 <sridharswain25@hotmail.com>
2017-07-03 11:41:36 +0530

    Async Home load removed, logs for home load added

--- api/login/Submitter.js
+++ api/login/Submitter.js
@@ -20,15 +20,16 @@ exports.submit=function(app,data,callback){
       }
       else{
         const onHomeLoad= function(response){
+          const afterHomeLoad = Date.now();
+          console.log("Home Loaded in : "+(afterHomeLoad-beforeHomeLoad).toString()+"ms");
+          var cookieSplit = response.request.headers.cookie.split(";");
+          cache.put(data.reg+data.password,response.request.headers.cookie,config.validity*1000*60);
+          callback(false,{token: cookieSplit[0], regno: cookieSplit[1].trim()});
         }
-
+        const beforeHomeLoad = Date.now();
         unirest.get("https://academicscc.vit.ac.in/student/stud_home.asp")
         .jar(CookieJar)
         .end(onHomeLoad);
-
-        var cookieSplit = response.request.headers.cookie.split(";");
-        cache.put(data.reg+data.password,response.request.headers.cookie,config.validity*1000*60);
-        callback(false,{token: cookieSplit[0], regno: cookieSplit[1].trim()});
     }
   }
   catch(e){
