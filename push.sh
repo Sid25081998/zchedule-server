dedd1ab80be1994ad8bb2e245a11eda13c734fe2
Sid25081998 <sridharswain25@hotmail.com>
2017-07-11 22:32:40 +0530

    New Servers added

--- push.sh
+++ push.sh
@@ -1,2 +1,10 @@
-git push server1
-git push server2
+git push server1 & git push server11 &\
+git push server2 & git push server12 &\
+git push server3 & git push server13 &\
+git push server4 & git push server14 &\
+git push server5 & git push server15 &\
+git push server6 & git push server16 &\
+git push server7 & git push server17 &\
+git push server8 & git push server18 &\
+git push server9 & git push server19 &\
+git push server10 & git push server20
